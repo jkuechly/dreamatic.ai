61877b77e8ff9a46c082b6c547d66f3c4f8cb147
jkuechly <josh@audacityventures.co>
2024-07-26 14:23:42 -0400

    Create pagination.js

---
+++ js/pagination.js
@@ -0,0 +1,29 @@
+let currentPage = 1;
+const resultsPerPage = 10;
+
+export function setupPagination(filteredResults, displayFunction) {
+    const totalPages = Math.ceil(filteredResults.length / resultsPerPage);
+    
+    function updatePaginationControls() {
+        const paginationDiv = document.getElementById('pagination');
+        paginationDiv.innerHTML = `
+            <button onclick="window.changePage(-1)" ${currentPage === 1 ? 'disabled' : ''}>Previous</button>
+            <span id="currentPage">Page ${currentPage} of ${totalPages}</span>
+            <button onclick="window.changePage(1)" ${currentPage === totalPages ? 'disabled' : ''}>Next</button>
+        `;
+    }
+
+    window.changePage = function(delta) {
+        const newPage = currentPage + delta;
+        if (newPage >= 1 && newPage <= totalPages) {
+            currentPage = newPage;
+            const startIndex = (currentPage - 1) * resultsPerPage;
+            const endIndex = startIndex + resultsPerPage;
+            const pageResults = filteredResults.slice(startIndex, endIndex);
+            displayFunction(pageResults, currentPage, totalPages);
+            updatePaginationControls();
+        }
+    };
+
+    updatePaginationControls();
+}
