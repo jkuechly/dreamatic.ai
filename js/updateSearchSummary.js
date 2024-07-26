5a84c8d11a459edd9eafec2f406de997789ea429
jkuechly <josh@audacityventures.co>
2024-07-26 14:17:22 -0400

    Update updateSearchSummary.js

--- js/updateSearchSummary.js
+++ js/updateSearchSummary.js
@@ -3,7 +3,7 @@ export function updateSearchSummary(searchParams, totalResults) {
     const { keyword, location, radius } = searchParams;
 
     searchSummary.innerHTML = `
-        <p>Showing ${totalResults} results for "${keyword}" 
+        <p>Showing ${totalResults} result${totalResults !== 1 ? 's' : ''} for "${keyword}" 
         within ${radius} miles of ${location}</p>
     `;
 }
