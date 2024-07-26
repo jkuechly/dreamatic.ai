6c547d4717b52602ecddf2479f8aa779f7dbfc90
jkuechly <josh@audacityventures.co>
2024-07-26 14:08:03 -0400

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -20,7 +20,6 @@ export function addMarkersToMap(results) {
     }
     clearMarkers();
     results.forEach((result) => {
-        // TODO: Update to google.maps.marker.AdvancedMarkerElement when available
         const marker = new google.maps.Marker({
             position: { lat: parseFloat(result.latitude), lng: parseFloat(result.longitude) },
             map: map,
@@ -55,4 +54,22 @@ export function addMarkersToMap(results) {
 export function clearMarkers() {
     markers.forEach((marker) => marker.setMap(null));
     markers = [];
+}
+
+export function updateMapWithFilteredResults(filteredResults, anchorLat, anchorLng, filterRadius) {
+    clearMarkers();
+    addMarkersToMap(filteredResults);
+
+    const radiusCircle = new google.maps.Circle({
+        strokeColor: '#0000FF',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#0000FF',
+        fillOpacity: 0.1,
+        map: map,
+        center: { lat: anchorLat, lng: anchorLng },
+        radius: filterRadius * 1609.34 // Convert miles to meters
+    });
+
+    map.fitBounds(radiusCircle.getBounds());
 }
