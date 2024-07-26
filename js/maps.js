81c8ef4ca5d7f482ac9a3f3208fdb326f04b36eb
jkuechly <josh@audacityventures.co>
2024-07-26 13:37:29 -0400

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -1,14 +1,14 @@
 let map;
 let markers = [];
 
-function initMap() {
+export function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 37.0902, lng: -95.7129 },
         zoom: 4,
     });
 }
 
-function addMarkersToMap(results) {
+export function addMarkersToMap(results) {
     clearMarkers();
     results.forEach((result) => {
         const marker = new google.maps.Marker({
@@ -42,9 +42,9 @@ function addMarkersToMap(results) {
     }
 }
 
-function clearMarkers() {
+export function clearMarkers() {
     markers.forEach((marker) => marker.setMap(null));
     markers = [];
 }
-window.map = map;
-google.maps.event.addDomListener(window, "load", initMap);
+
+window.initMap = initMap;
