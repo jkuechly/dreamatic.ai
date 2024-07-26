339741afbb07c6f303dc53e4375b615500106c51
jkuechly <josh@audacityventures.co>
2024-07-26 13:28:50 -0400

    Create switchToResultsView.js

---
+++ js/switchToResultsView.js
@@ -0,0 +1,4 @@
+function switchToResultsView() {
+        document.getElementById('searchContainer').style.display = 'none';
+        document.getElementById('resultsContainer').style.display = 'flex';
+    }
